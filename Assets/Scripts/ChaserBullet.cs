64741838cf95eafa9b9758889be9615ad5bde1df
Samuel Proulx <proulxsamuel@gmail.com>
2019-03-16 09:59:46 -0400

    #3 Implement chaser shooting at reticle

--- Assets/Scripts/ChaserBullet.cs
+++ Assets/Scripts/ChaserBullet.cs
@@ -5,29 +5,11 @@ using UnityEngine.UI;
 
 public class ChaserBullet : MonoBehaviour
 {
-    private Rigidbody rb;
-    public Image reticle;
-
-    // Start is called before the first frame update
-    void Start()
+    void OnCollisionEnter(Collision collision)
     {
-        rb = GetComponent<Rigidbody>();
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
-
-        
-
-        
-    }
-
-    void Shoot() {
-
+        if (collision.gameObject.tag == "Terrain")
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/ChaserController.cs
+++ Assets/Scripts/ChaserController.cs
@@ -5,13 +5,26 @@ using UnityEngine.UI;
 
 public class ChaserController : MonoBehaviour
 {
+    // Input
     public string inputAxis = "Horizontal";
+    public string inputFire = "Fire1";
+   
+    // UI references
     public Canvas canvas;
     public Image reticle;
+    
+    // Aiming parameters
     public float maxAimSpeed = 200;
     public float aimAccelaration = 100;
     public float aimDecelaration = 0.02f;
     public float maxReticlePosition = 400f;
+
+    public GameObject bullet;
+    public Vector3 bulletOffset = Vector3.zero + Vector3.back;
+    public float targetingOffset = 2.0f;
+    public float bulletSpeed = 25.0f;
+
+    public Transform player;
 
     private Rigidbody rb;
     private float aimSpeed = 0;
@@ -27,6 +40,15 @@ public class ChaserController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MoveReticle();
+        if (Input.GetButtonDown(inputFire))
+        {
+            Shoot();
+        }
+    }
+
+    void MoveReticle()
+    {
         float x = Input.GetAxis(inputAxis);
         aimSpeed *= (1 - (1 - aimDecelaration) * Time.deltaTime);
         aimSpeed += x * aimAccelaration * Time.deltaTime;
@@ -38,5 +60,27 @@ public class ChaserController : MonoBehaviour
         xCoord = Mathf.Min(xCoord, maxReticlePosition);
         xCoord = Mathf.Max(xCoord, -maxReticlePosition);
         reticle.rectTransform.anchoredPosition = new Vector2(xCoord, reticleHeight);
+    }
+
+    void Shoot()
+    {
+        // Find target position
+        Vector2 canvasPosition = canvas.GetComponent<RectTransform>().anchoredPosition
+            + reticle.rectTransform.anchoredPosition;
+        Vector2 pixelPosition = RectTransformUtility.PixelAdjustPoint(canvasPosition, transform, canvas);
+        float distance = Vector3.Distance(player.position, Camera.main.transform.position);
+        Ray ray = Camera.main.ScreenPointToRay(new Vector3(pixelPosition.x, pixelPosition.y, 0.0f));
+        RaycastHit hit;
+        Physics.Raycast(ray, out hit);
+        Vector3 targetPosition = new Vector3(
+            hit.point.x,
+            player.position.y,
+            player.position.z
+        );
+
+        // Shoot
+        Vector3 direction = (targetPosition + Vector3.up * targetingOffset - transform.position).normalized;
+        GameObject instance = Instantiate(bullet, transform.position + bulletOffset, Quaternion.identity);
+        instance.GetComponent<Rigidbody>().velocity = direction * bulletSpeed;
     }
 }
