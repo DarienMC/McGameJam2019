9f5d04345c22ad962dd35487f8051eb5303ea9a8
Darien Muse-Charbonneau <darienmusecharbonneau@gmail.com>
2019-03-16 14:48:37 -0400

    #14 Added GManager win states
    Added the win state conditions in GManager

--- Assets/Scripts/GManager.cs
+++ Assets/Scripts/GManager.cs
@@ -5,6 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class GManager : MonoBehaviour
 {
+
+    //Timer elements
+    public float timerLimit = 400.0f;
+
+    //References
+    public GameObject runner;
+    public GameObject chaser;
+
+    private float timePassed = 0.0f;
+    private float playerDistance;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,11 +25,35 @@ public class GManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timePassed += Time.time;
+        playerDistance = chaser.transform.position.z - runner.transform.position.z;
+
+        //Win States
+        if (playerDistance <= chaser.GetComponent<Player2Movement>().minZDistance) {
+            PlayerDeath();
+        }
+
+        if (playerDistance > chaser.GetComponent<Player2Movement>().maxZDistance) {
+            PlayerWin();
+        }
+
+        if (timerLimit <= timePassed) {
+            PlayerDeath();
+        }
+
     }
 
     public void PlayerDeath()
     {
+        StartCoroutine(WaitForPlayerDeath());
+    }
+
+    public void PlayerWin() {
+        SceneManager.LoadScene(0);
+    }
+
+    private IEnumerator WaitForPlayerDeath() {
+        yield return new WaitForSeconds(5);
         SceneManager.LoadScene(0);
     }
 }
