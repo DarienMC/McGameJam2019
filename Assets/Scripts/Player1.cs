e24231ff82fdcb361a9c6e9ca4700a2af42807ab
Jesus Imery <yisas_0929@hotmail.com>
2019-03-15 21:33:30 -0400

    #1 Player basic jump

--- Assets/Scripts/Player1.cs
+++ Assets/Scripts/Player1.cs
@@ -6,11 +6,15 @@ using UnityEngine;
 public class Player1 : MonoBehaviour
 {
     public float speed;
+    public float jumpForce;
 
     // Input variables
     private float horizontal;
+    private bool jump;
 
     private Rigidbody rb;
+
+    private bool grounded = false;
     private int lastMovementDirection = 1; // -1 left, 0 none, 1 right
 
     // Start is called before the first frame update
@@ -23,11 +27,18 @@ public class Player1 : MonoBehaviour
     void Update()
     {
         horizontal = Input.GetAxis("Horizontal");
+        jump = Input.GetButtonDown("Jump");
     }
 
     private void FixedUpdate()
     {
         rb.AddForce(new Vector3(horizontal * speed, 0, 0));
+
+        if(jump && grounded)
+        {
+            rb.AddForce(new Vector3(0, jumpForce, 0));
+            grounded = false;
+        }
     }
 
     private void LateUpdate()
@@ -37,8 +48,16 @@ public class Player1 : MonoBehaviour
             if (lastMovementDirection != Mathf.Sign(horizontal))
             {
                 lastMovementDirection *= -1;
-                rb.velocity = new Vector3(0, 0, 0);
+                rb.velocity = new Vector3(0, rb.velocity.y, 0);
             }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.layer == 9)
+        {
+            grounded = true;
         }
     }
 }
