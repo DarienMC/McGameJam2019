3659e53dbd372d606085f3918dfbe162051015a6
Samuel Proulx <proulxsamuel@gmail.com>
2019-03-16 08:27:27 -0400

    #3 Move reticle position and prepare scene for shooting

--- Assets/Scripts/ChaserController.cs
+++ Assets/Scripts/ChaserController.cs
@@ -5,35 +5,38 @@ using UnityEngine.UI;
 
 public class ChaserController : MonoBehaviour
 {
+    public string inputAxis = "Horizontal";
     public Canvas canvas;
     public Image reticle;
     public float maxAimSpeed = 200;
     public float aimAccelaration = 100;
     public float aimDecelaration = 0.02f;
-    public float maxReticlePosition = 500f;
+    public float maxReticlePosition = 400f;
 
     private Rigidbody rb;
     private float aimSpeed = 0;
+    private float reticleHeight = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        reticleHeight = reticle.rectTransform.anchoredPosition.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
+        float x = Input.GetAxis(inputAxis);
         aimSpeed *= (1 - (1 - aimDecelaration) * Time.deltaTime);
         aimSpeed += x * aimAccelaration * Time.deltaTime;
         aimSpeed = Mathf.Min(aimSpeed, maxAimSpeed);
         aimSpeed = Mathf.Max(aimSpeed, -maxAimSpeed);
         
-        reticle.rectTransform.anchoredPosition += new Vector2(aimSpeed*Time.deltaTime, 0);
+        reticle.rectTransform.anchoredPosition += new Vector2(aimSpeed*Time.deltaTime, reticleHeight);
         float xCoord = reticle.rectTransform.anchoredPosition.x;
         xCoord = Mathf.Min(xCoord, maxReticlePosition);
         xCoord = Mathf.Max(xCoord, -maxReticlePosition);
-        reticle.rectTransform.anchoredPosition = new Vector2(xCoord, 0);
+        reticle.rectTransform.anchoredPosition = new Vector2(xCoord, reticleHeight);
     }
 }
