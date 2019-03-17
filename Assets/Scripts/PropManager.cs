4bdc841c6c07113e79d0d2bc83169fa8c871fb36
Sabina.Sasu <oana.sasu@mail.mcgill.ca>
2019-03-17 10:11:54 -0400

    balanced terrain generation (added spawning restrictions)

--- Assets/Scripts/PropManager.cs
+++ Assets/Scripts/PropManager.cs
@@ -12,16 +12,18 @@ public class PropManager : MonoBehaviour
     public GameObject texture;
     //must be in percentage
     public int spawingObjectSliceChance;
+    public int distanceBetweenBeers;
 
     private int maxObjectsOnASlice = 7;
-    private int nbrPresets = 30;
+    private int nbrPresets = 25;
     private float widthTerrain;
     private enum Obstacle { beer, jumpObs1, jumpObs2, avoidObs, nothing, jumpBeerObs };
-
+    private int beerDelay = 0;
 
     Obstacle[,] array = new Obstacle[,]
     {
-        {Obstacle.beer, Obstacle.nothing, Obstacle.nothing, Obstacle.jumpObs2, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing},
+        {Obstacle.nothing, Obstacle.jumpObs2, Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.avoidObs},
+        {Obstacle.beer, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing},
         {Obstacle.nothing, Obstacle.beer, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing},
         {Obstacle.nothing, Obstacle.nothing, Obstacle.beer, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing},
         {Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.beer, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing},
@@ -29,27 +31,21 @@ public class PropManager : MonoBehaviour
         {Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.beer, Obstacle.nothing},
         {Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.beer},
         {Obstacle.beer, Obstacle.nothing, Obstacle.nothing, Obstacle.beer, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing},
-        {Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.beer, Obstacle.nothing, Obstacle.beer},
+        {Obstacle.nothing, Obstacle.nothing, Obstacle.beer, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.beer},
         {Obstacle.beer, Obstacle.nothing, Obstacle.nothing, Obstacle.beer, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing},
-        {Obstacle.jumpObs2, Obstacle.nothing, Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.avoidObs},
-        {Obstacle.avoidObs, Obstacle.jumpObs1,  Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.avoidObs,  Obstacle.avoidObs},
-        {Obstacle.nothing, Obstacle.avoidObs,  Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.avoidObs,  Obstacle.jumpBeerObs},
-        {Obstacle.nothing, Obstacle.avoidObs,  Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.avoidObs,  Obstacle.jumpBeerObs},
-        {Obstacle.jumpObs2, Obstacle.nothing,  Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing},
-        {Obstacle.avoidObs, Obstacle.avoidObs,  Obstacle.jumpObs1, Obstacle.nothing, Obstacle.avoidObs, Obstacle.nothing,  Obstacle.avoidObs},
-        {Obstacle.avoidObs, Obstacle.nothing,  Obstacle.nothing, Obstacle.avoidObs, Obstacle.jumpObs1, Obstacle.jumpObs2,  Obstacle.nothing},
-        {Obstacle.jumpBeerObs, Obstacle.jumpObs1,  Obstacle.jumpObs2, Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.nothing,  Obstacle.nothing},
-        {Obstacle.avoidObs, Obstacle.nothing,  Obstacle.avoidObs, Obstacle.nothing, Obstacle.avoidObs, Obstacle.nothing,  Obstacle.nothing},
-        {Obstacle.beer, Obstacle.nothing,  Obstacle.avoidObs, Obstacle.beer, Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing},
-        {Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing},
-        {Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing},
-        {Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing},
-        {Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing},
-        {Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing},
-        {Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing},
-        {Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing},
-        {Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing},
-        {Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing, Obstacle.nothing, Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing}
+        {Obstacle.nothing, Obstacle.jumpObs2, Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.avoidObs},
+        {Obstacle.avoidObs, Obstacle.nothing,  Obstacle.jumpObs2, Obstacle.nothing, Obstacle.jumpObs2, Obstacle.avoidObs,  Obstacle.avoidObs},
+        {Obstacle.nothing, Obstacle.avoidObs,  Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.jumpObs1,  Obstacle.jumpBeerObs},
+        {Obstacle.nothing, Obstacle.nothing,  Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.avoidObs,  Obstacle.jumpBeerObs},
+        {Obstacle.nothing, Obstacle.jumpObs2,  Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.jumpObs2, Obstacle.nothing,  Obstacle.nothing},
+        {Obstacle.avoidObs, Obstacle.avoidObs,  Obstacle.jumpObs1, Obstacle.nothing, Obstacle.avoidObs, Obstacle.avoidObs,  Obstacle.jumpObs1},
+        {Obstacle.avoidObs, Obstacle.nothing,  Obstacle.nothing, Obstacle.avoidObs, Obstacle.jumpObs2, Obstacle.nothing,  Obstacle.nothing},
+        {Obstacle.jumpBeerObs, Obstacle.nothing,  Obstacle.jumpObs2, Obstacle.jumpObs1, Obstacle.avoidObs, Obstacle.nothing,  Obstacle.nothing},
+        {Obstacle.avoidObs, Obstacle.nothing,  Obstacle.avoidObs, Obstacle.nothing, Obstacle.nothing, Obstacle.jumpObs2,  Obstacle.avoidObs},
+        {Obstacle.jumpBeerObs, Obstacle.jumpObs1,  Obstacle.avoidObs, Obstacle.avoidObs, Obstacle.nothing, Obstacle.beer,  Obstacle.nothing},
+        {Obstacle.avoidObs, Obstacle.avoidObs,  Obstacle.nothing, Obstacle.jumpObs2, Obstacle.jumpObs1, Obstacle.nothing,  Obstacle.jumpObs1},
+        {Obstacle.nothing, Obstacle.jumpObs2,  Obstacle.nothing, Obstacle.jumpObs2, Obstacle.avoidObs, Obstacle.avoidObs,  Obstacle.nothing},
+        {Obstacle.avoidObs, Obstacle.avoidObs,  Obstacle.avoidObs, Obstacle.jumpObs1, Obstacle.nothing, Obstacle.nothing,  Obstacle.nothing}
     };
 
     void Start()
@@ -65,6 +61,7 @@ public class PropManager : MonoBehaviour
         {
             instance.transform.SetParent(null);
             Destroy(instance);
+            Debug.Log("Destroyed");
         }
     }
 
@@ -93,6 +90,7 @@ public class PropManager : MonoBehaviour
 
     public void GenerateLine(Transform slice)
     {
+        
         int isSpawingLine = Random.Range(0, 100);
 
         //spawn obstacles
@@ -100,6 +98,7 @@ public class PropManager : MonoBehaviour
         {
             //getting a preset
             int typeOfTerrain = Random.Range(0, nbrPresets - 1);
+            bool foundBeer = false;
 
             for (int i = 0; i < maxObjectsOnASlice; ++i)
             {
@@ -120,15 +119,30 @@ public class PropManager : MonoBehaviour
 
                 else if (array[typeOfTerrain, i] == Obstacle.beer)
                 {
-                    GenerateObstacle(powerUp, slice.GetChild(i).transform.position + Vector3.up * 0.2f);
+                    if (beerDelay >= distanceBetweenBeers)
+                    {
+                        GenerateObstacle(powerUp, slice.GetChild(i).transform.position + Vector3.up * 0.2f);
+                        foundBeer = true;
+                        beerDelay = 0;
+                    }
+                    else
+                        this.GenerateObstacle(cone, slice.GetChild(i).transform.position + Vector3.up * 0.2f);
                 }
 
                 else if (array[typeOfTerrain, i] == Obstacle.jumpBeerObs)
                 {
-                    GameObject instance2 = Instantiate(powerUp, slice.GetChild(i).transform.position + Vector3.up * 2, Quaternion.identity);
-                    terrain.AttachProp(instance2.transform);
+                    ReplaceFloor(porthole1, i);
+                    if (beerDelay >= distanceBetweenBeers)
+                    {
+                        GameObject instance2 = Instantiate(powerUp, slice.GetChild(i).transform.position + Vector3.up * 2, Quaternion.identity);
+                        terrain.AttachProp(instance2.transform);
+                        foundBeer = true;
+                        beerDelay = 0;
+                    }
                 }
             }
+            if (foundBeer == false)
+                beerDelay++;
         }
     }
 }
