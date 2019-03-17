fe8ee9d14989ad42a06a4de3a4e0787aa589f8a6
Samuel Proulx <proulxsamuel@gmail.com>
2019-03-17 09:14:38 -0400

    #36 Fix obstacle spawning

--- Assets/Scripts/PropManager.cs
+++ Assets/Scripts/PropManager.cs
@@ -61,6 +61,11 @@ public class PropManager : MonoBehaviour
     {
         GameObject instance = Instantiate(gameobject, gameObject.transform.position + offset, gameobject.transform.rotation);
         terrain.AttachProp(instance.transform);
+        if (instance.transform.position.x > 6 || instance.transform.position.x < -6)
+        {
+            instance.transform.SetParent(null);
+            Destroy(instance);
+        }
     }
 
     void ReplaceFloor(GameObject gameObject, int childIndex)
@@ -78,8 +83,8 @@ public class PropManager : MonoBehaviour
         GameObject replacement = Instantiate(gameObject, gameObject.transform.position, gameObject.transform.rotation);
         Transform left = terrain.previousSlice.GetChild(childIndex - 1);
         Transform right = terrain.previousSlice.GetChild(childIndex);
-        replacement.transform.position = left.position + new Vector3(0.0f, 0.0f, 1.0f) * terrain.sliceLength;
-        replacement.transform.parent = left.parent;
+        replacement.transform.position = right.position + new Vector3(0.0f, 0.0f, 1.0f) * terrain.sliceLength;
+        replacement.transform.parent = right.parent;
         left.SetParent(null);
         Destroy(left.gameObject);
         right.SetParent(null);
@@ -96,26 +101,26 @@ public class PropManager : MonoBehaviour
             //getting a preset
             int typeOfTerrain = Random.Range(0, nbrPresets - 1);
 
-            for (int i = 0; i < maxObjectsOnASlice; i++)
+            for (int i = 0; i < maxObjectsOnASlice; ++i)
             {
                 if (array[typeOfTerrain, i] == Obstacle.jumpObs1)
                 {
                     ReplaceFloor(porthole1, i);
                 }
 
-                //else if (array[typeOfTerrain, i] == Obstacle.jumpObs2 && i % 2 == 1)
-                //{
-                //    ReplaceFloorLarge(porthole2, i);
-                //}
+                else if (array[typeOfTerrain, i] == Obstacle.jumpObs2 && i % 2 == 1)
+                {
+                    ReplaceFloorLarge(porthole2, i);
+                }
 
                 else if (array[typeOfTerrain, i] == Obstacle.avoidObs)
                 {
-                    GenerateObstacle(cone, slice.GetChild(i).transform.position);
+                    GenerateObstacle(cone, slice.GetChild(i).transform.position + Vector3.up * 0.2f);
                 }
 
                 else if (array[typeOfTerrain, i] == Obstacle.beer)
                 {
-                    GenerateObstacle(powerUp, slice.GetChild(i).transform.position);
+                    GenerateObstacle(powerUp, slice.GetChild(i).transform.position + Vector3.up * 0.2f);
                 }
 
                 else if (array[typeOfTerrain, i] == Obstacle.jumpBeerObs)
